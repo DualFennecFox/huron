1eb2aa82e116bbd934b56582aea46769a5e0d4dd
Firulais <gustavoperezsoto18@gmail.com>
2020-06-16 19:06:43 -0400

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -2,7 +2,11 @@ const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags"); 
 
 module.exports.run = async (client, message, args) => {
+    if (args[0]) {
+        return getCMD(client, message, args[0]);
+    } else {
     getAll(client, message);
+}
 }
 
 function getAll(client, message) {
@@ -23,6 +27,31 @@ const info = client.categories
     return message.channel.send(embed.setDescription(info));
 }
  
+function getCMD(client, message, input) {
+    const embed = new RichEmbed()
+
+    // Get the cmd by the name or alias
+    const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()));
+    
+    let info = `No information found for command **${input.toLowerCase()}**`;
+
+    // If no cmd is found, send not found embed
+    if (!cmd) {
+        return message.channel.send(embed.setColor("RED").setDescription(info));
+    }
+
+    // Add all cmd info to the embed
+    if (cmd.name) info = `**Command name**: ${cmd.name}`;
+    if (cmd.aliases) info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
+    if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+    if (cmd.usage) {
+        info += `\n**Usage**: ${cmd.usage}`;
+        embed.setFooter(`Syntax: <> = required, [] = optional`);
+    }
+
+    return message.channel.send(embed.setColor("GREEN").setDescription(info));
+}
+
 module.exports.help = {
     name: "help",
     aliases: ['Help', 'HELP'],
