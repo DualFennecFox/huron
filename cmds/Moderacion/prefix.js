1c3dc165abc6a499be3445e50349f2edf880ec3c
Firulais <gustavoperezsoto18@gmail.com>
2020-06-18 01:41:56 -0400

    Update prefix.js

--- cmds/Moderacion/prefix.js
+++ cmds/Moderacion/prefix.js
@@ -7,9 +7,9 @@ module.exports = {
     examples: ['!prefix -', '!prefix --='],
     run: async (client,message,args,db,prefix) => {
     if(!message.member.hasPermission("KICK_MEMBERS", "BAN_MEMBERS", "ADMINISTRATOR", "MANAGE_ROLES") || !message.guild.owner) return message.channel.send("No tienes permisos para usar este comando!");
-    if (args.length === 0){
+    if (args[0]) {
         message.channel.send(`Mi prefix en este server es ${prefix}`);
-    } else if (args.length === 1){
+    } else if (args[1]){
         let nPrefix = args[0];
 
         db.collection('guilds').doc(message.guild.id).update({
