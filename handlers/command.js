8a3ab1f2a87feb5d33735acf86e6d3b59a977181
Firulais <gustavoperezsoto18@gmail.com>
2020-06-17 22:27:55 -0400

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -6,11 +6,11 @@ let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    readdirSync("./cmds/").forEach(dir => {
+        const commands = readdirSync(`./cmds/${dir}/`).filter(file => file.endsWith(".js"));
     
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+            let pull = require(`../cmds/${dir}/${file}`);
     
             if (pull.name) {
                 client.commands.set(pull.name, pull);
