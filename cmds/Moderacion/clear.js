b38b0d321000e88238b3645fe3c921eb59f49ab8
Firulais <gustavoperezsoto18@gmail.com>
2020-06-21 18:49:33 -0400

    Update clear.js

--- cmds/Moderacion/clear.js
+++ cmds/Moderacion/clear.js
@@ -11,7 +11,6 @@ const Discord = require('discord.js');
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tienes permisos para usar este comando!");
     if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tengo permisos para borrar mensajes!");
     if(!args[0]) return message.channel.send("Dime cuantos mensajes quieres borrar!");
-    let messagesDeleted = await clearChannel(message.channel);
 
     let collected = await channel.messages.fetch();
   if (collected.size > 0) {
