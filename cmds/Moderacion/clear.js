c534c2ffafc5412539cdac4874cc8dbdaaaf9cc1
Firulais <gustavoperezsoto18@gmail.com>
2020-06-21 17:48:06 -0400

    Create clear.js

---
+++ cmds/Moderacion/clear.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+    module.exports  = {
+    name : 'clear',
+    category: "Moderacion",
+    description : 'Este comando borra un número de mensajes seleccionados por el usuario',
+    aliases: ['Clear', 'CLEAR'],
+    usage: '!clear',
+    examples: ['!clear 50'],
+    run: async (client, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tienes permisos para usar este comando!");
+    if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tengo permisos para borrar mensajes!");
+    if(!args[0]) return message.channel.send("Dime cuantos mensajes quieres borrar!");
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Borrados ${args[0]} mensajes`).then(msg => msg.delete(5000));
+    })
+    }
+}
