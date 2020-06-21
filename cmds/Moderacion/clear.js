575a486369464fc3b0c765cbd27ccbe7b60013f5
Firulais <gustavoperezsoto18@gmail.com>
2020-06-21 18:07:29 -0400

    Update clear.js

--- cmds/Moderacion/clear.js
+++ cmds/Moderacion/clear.js
@@ -18,11 +18,11 @@ const Discord = require('discord.js');
 }
 
 async function clearChannel(channel, n = 0, old = false) {
-  let collected = await channel.fetchMessages();
+  let collected = await channel.messages.fetch();
   if (collected.size > 0) {
     if (old) {
-      for (let msg of collected.array()) {
-        await msg.delete();
+      for (let message of collected.array()) {
+        await message.delete();
         n++;
       }
     } else {
