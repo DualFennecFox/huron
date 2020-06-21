f22146e5c769401ba461ea959469ca01cd70c04a
Firulais <gustavoperezsoto18@gmail.com>
2020-06-21 17:07:22 -0400

    Create Fetchuser.js

---
+++ cmds/Info/Fetchuser.js
@@ -0,0 +1,15 @@
+
+   module.exports = {
+    name : 'pi2g',
+    category: "Info",
+    description : 'Un comando b2sico que sirve para probar el bot, si escribes "!ping" este dira "Pong"',
+    aliases: ['P2ng', 'P2NG'],
+    usage: '!p2ng',
+   run: async (client, message, args) => {
+
+    let gUser = message.guild.member(client.users.Fetch(args[0]))
+
+    message.channel.send(`<@${gUser.id}> ID ${gUser.id}`);
+    
+}
+}
