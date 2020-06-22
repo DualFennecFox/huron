d9fa633e69f9b078761e3289403c4dc07218874f
Firulais <gustavoperezsoto18@gmail.com>
2020-06-22 01:42:41 -0400

    Update clear.js

--- cmds/Moderacion/clear.js
+++ cmds/Moderacion/clear.js
@@ -12,6 +12,8 @@ const Discord = require('discord.js');
     if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tengo permisos para borrar mensajes!");
     if(!args[0]) return message.channel.send("Dime cuantos mensajes quieres borrar!");
     
+  
+    async function clearChannel(channel, n = 0, old = false) {
     let collected = await channel.messages.fetch();
     if (collected.size > 0) {
       if (old) {
@@ -24,12 +26,14 @@ const Discord = require('discord.js');
         if (deleted.size < collected.size) old = true;
         n += deleted
         message.channel.bulkDelete(100, old = true);
-      messagesDeleted = messages.array().length;
+
+      let messagesDeleted = messages.array().length;
       
-    message.channel.send(`Se han borrado ${messagesDeleted} mensages`);
       }
   
       return n + await clearChannel(channel, old);;
-    } else return 0;
+    }
+    message.channel.send(`Se han borrado ${deleted} mensages`);
   }
 }
+    }
