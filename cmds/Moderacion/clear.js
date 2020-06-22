e3789d92099d140aa4b08ca5cf1190572a251252
Firulais <gustavoperezsoto18@gmail.com>
2020-06-22 04:02:08 -0400

    Update clear.js

--- cmds/Moderacion/clear.js
+++ cmds/Moderacion/clear.js
@@ -8,28 +8,27 @@ const Discord = require('discord.js');
     usage: '!clear',
     examples: ['!clear 50'],
     run: async (client, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tienes permisos para usar este comando!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES") || !message.guild.owner) return message.channel.send("No tienes permisos para usar este comando!");
     if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tengo permisos para borrar mensajes!");
     if(!args[0]) return message.channel.send("Dime cuantos mensajes quieres borrar!");
-    
+      let messagesDeleted = await clearChannel(message.channel);
     }
     }
+
     async function clearChannel(channel, n = 0, old = false) {
-    let collected = await channel.messages.fetch();
-    if (collected.size > 0) {
-      if (old) {
-        for (let message of collected.array()) {
-          await message.delete().then(`Se han borrado ${collected} mensages`)
-          n++;
+      let collected = await channel.fetchMessages();
+      if (collected.size > 0) {
+        if (old) {
+          for (let msg of collected.array()) {
+            await msg.delete();
+            n++;
+          }
+        } else {
+          let deleted = await channel.bulkDelete(100, true);
+          if (deleted.size < collected.size) old = true;
+          n += deleted;
         }
-      } else {
-        let deleted = await channel.bulkDelete(100, true);
-        if (deleted.size < collected.size) old = true;
-        n += deleted
-        message.channel.bulkDelete(100, old = true).then(`Se han borrado ${deleted} mensages`)
-      
-      }
-  
-      return n + await clearChannel(channel, old);
+    
+        return n + await clearChannel(channel, old);
+      } else return 0;
     }
-  }
