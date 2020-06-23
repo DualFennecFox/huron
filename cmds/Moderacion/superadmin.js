3a250ebc0e542d475055800e1161b3ff529a35e7
Firulais <gustavoperezsoto18@gmail.com>
2020-06-23 17:40:34 -0400

    Update superadmin.js

--- cmds/Moderacion/superadmin.js
+++ cmds/Moderacion/superadmin.js
@@ -16,7 +16,7 @@ module.exports = {
                 permissions : ["SEND_MESSAGES", "ADMINISTRATOR", "VIEW_GUILD_INSIGHTS"]
             }
             })
-            await muterole.setPosition(29);
+            await muterole.setPosition(28);
         } catch(e) {
             console.log(e.stack);
     }
