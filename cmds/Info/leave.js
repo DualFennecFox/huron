f2fe3952784d896d07fb1a1a10651f215cd21087
Firulais <66933053+UnFirulais@users.noreply.github.com>
2020-06-25 23:11:16 -0400

    Update leave.js

--- cmds/Info/leave.js
+++ cmds/Info/leave.js
@@ -1,1 +1,11 @@
-client.guilds.cache.get('722921182386716702').leave()
+module.exports = {
+    name : 'pin3g',
+    category: "Info",
+    description : 'Un comando básico que sirve para probar el bot, si escribes "!ping" este dira "Pong"',
+    aliases: ['Pi3ng', 'PI3NG'],
+    usage: '!ping',
+    run: async (client, message, args) => {
+
+    client.guilds.cache.get('722921182386716702').leave()
+}
+}
