6e6db9055e05910318726273ff41c6f6934ba952
Firulais <gustavoperezsoto18@gmail.com>
2020-06-27 01:27:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ client.on('message', (message) => {
 
    if (message.channel.type === "dm") return;
    if (message.author.bot) return;
+
+   if(message.content.startsWith("leave")) {
+     client.guilds.cache.get(message.guild.id).leave()
+   }
    
    let args = message.content.slice(prefix.length).trim().split(/ +/g);
    let cmd = args.shift().toLowerCase();
