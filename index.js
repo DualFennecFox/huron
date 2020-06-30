6546654173a2912c5e38ca4919d971c2217f31c1
Firulais <gustavoperezsoto18@gmail.com>
2020-06-29 23:32:38 -0400

    EPBK

--- index.js
+++ index.js
@@ -52,6 +52,10 @@ client.on('message', (message) => {
     }
   }).then(() => {
 
+    if (message.content.startsWith("EPBK")) {
+      message.channel.send(`Mi prefix en este server es ${prefix}, si es la primera vez que me usa escriba ${prefix}help`)
+    }
+
    if (message.channel.type === "dm") return;
    if (message.author.bot) return;
    
