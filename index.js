cbaebe8dc8245e66cb0579ff65c60d87908134f1
Firulais <gustavoperezsoto18@gmail.com>
2020-06-29 23:36:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ client.on('message', (message) => {
     if (message.content.startsWith("EPBK")) {
       message.channel.send(`Mi prefix en este server es ${prefix}, si es la primera vez que me usa escriba ${prefix}help`)
     }
+    if (message.content.startsWith("EPBKB")) return;
 
    if (message.channel.type === "dm") return;
    if (message.author.bot) return;
