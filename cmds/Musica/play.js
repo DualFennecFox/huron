e10c2dc157b984fabb84de4636acbcec6571d3a4
Firulais <gustavoperezsoto18@gmail.com>
2020-07-28 22:33:08 -0400

    Update play.js

--- cmds/Musica/play.js
+++ cmds/Musica/play.js
@@ -241,9 +241,9 @@ function playSong(queue, message) {
                           .addField("\`9\`", vidNameArr[8])
                           .addField("\`10\`", vidNameArr[9])
                           .setFooter('Escribe "exit" para salir')
-                          var songEmbed = message.channel.send({ embed });
+                          var songEmbed = await message.channel.send({ embed });
                           try {
-                              var response = message.channel.awaitMessages(msg => (msg.content > 0 && msg.content < 11 || msg.content === 'exit') && msg.author.id === message.author.id, {max: 1, time: 60000, errors: ['time']})
+                              var response = await message.channel.awaitMessages(msg => (msg.content > 0 && msg.content < 11 || msg.content === 'exit') && msg.author.id === message.author.id, {max: 1, time: 60000, errors: ['time']})
                               if (response.first().content) musicData.server[message.guild.id].awaiting = false
                               var videoIndex = parseInt(response.first().content);
                               } catch (err) {
