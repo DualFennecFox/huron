da474689e48673ba4ef45fd33f327218abd131b3
Firulais <gustavoperezsoto18@gmail.com>
2020-07-29 22:35:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ client.on('guildMemberAdd', member => {
   if (!Channel) return
   if (!Channel.permissionsFor(member.guild.me).has("SEND_MESSAGES")) return
   
-  let msg = doc.LeaveMsg.replace("{user}", member).replace("{server}", member.guild.name).replace("{username}", member.user.tag).replace("{owner}", member.guild.owner.user.tag)
+  let msg = doc.LeaveMsg.replace("{user}", member).replace("{server}", member.guild.name).replace("{username}", member.user.tag).replace("{members}", member.guild.memberCount).replace("{owner}", member.guild.owner.user.tag)
  
  Channel.send(msg)
 }).catch(err => {
@@ -147,7 +147,7 @@ client.on('guildMemberRemove', member => {
     if (!Channel) return
     if (!Channel.permissionsFor(member.guild.me).has("SEND_MESSAGES")) return
 
-    let msg = doc.LeaveMsg.replace("{user}", member).replace("{server}", member.guild.name).replace("{username}", member.user.tag).replace("{owner}", member.guild.owner.user.tag)
+    let msg = doc.LeaveMsg.replace("{user}", member).replace("{server}", member.guild.name).replace("{username}", member.user.tag).replace("{members}", member.guild.memberCount).replace("{owner}", member.guild.owner.user.tag)
 
     Channel.send(msg)
   }).catch(err => {
