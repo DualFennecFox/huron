61179391ac2688d097c6120494126b5cfa98f9e0
Firulais <gustavoperezsoto18@gmail.com>
2020-07-30 18:15:23 -0400

    Update prefix.js

--- cmds/Moderacion/prefix.js
+++ cmds/Moderacion/prefix.js
@@ -1,29 +1,11 @@
-const Discord = require('discord.js');
-const mongoose = require('mongoose')
-const fs = require('fs')
-const Guild = require('./models/Guild')
-const { getGuild, updateGuild, createGuild } = require('./models/functions')
 module.exports = {
     name: "prefix",
     category: "Moderacion",
-    description : "Con este comando puedes ver tu prefix o cambiarlo elijiendo uno",
+    description : "Con este comando puedes ver el prefix en el servidor",
     aliases: ['Prefix', 'PREFIX'],
     usage: '!prefix',
     examples: ['!prefix -', '!prefix --='],
     run: async (client, message, args, prefix) => {
-            if (args.length === 0){
-            message.channel.send(`Mi prefix en este server es ${prefix}`);
-        } else if (args.length === 1){
-            if(!message.member.hasPermission("KICK_MEMBERS" || "BAN_MEMBERS" || "ADMINISTRATOR") || !message.guild.owner) return message.channel.send("No tienes permisos para usar este comando!")
-            let nPrefix = args.join(' ');
-
-  
-  await updateGuild(message.guild, { prefix: nPrefix });
-
-    message.channel.send(`Su nuevo Prefix es ${nPrefix}`)
-    .catch(err => {
-        console.log(err);
-    })
+            message.channel.send(`Mi prefix en este server es ${prefix}`);  
     }
-}
 }
