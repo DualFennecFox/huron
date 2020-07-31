11ed3edd68c297b97328cc251f7e1e4b652a3eed
Firulais <gustavoperezsoto18@gmail.com>
2020-07-30 22:55:04 -0400

    Update ban.js

--- cmds/Moderacion/ban.js
+++ cmds/Moderacion/ban.js
@@ -33,7 +33,7 @@ const Discord = require('discord.js');
     .addField("Usuario Baneado", `${User} Y su ID es ${User.id}`)
     .addField("Baneado Por", `<@!${message.author.id}> Y su ID es ${message.author.id}`)
     .addField("Razón de Baneo", bReason);
-    message.guild.member(User).ban(bReason)
+    message.guild.members.ban(User.id, { reason: bReason })
 
     message.channel.send( banEmbed )
     .catch(err => {
