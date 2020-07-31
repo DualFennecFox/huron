b115c6c5ebe5aaa464d1941d2c40403d8a661439
Firulais <gustavoperezsoto18@gmail.com>
2020-07-30 21:55:13 -0400

    Update ban.js

--- cmds/Moderacion/ban.js
+++ cmds/Moderacion/ban.js
@@ -9,30 +9,31 @@ const Discord = require('discord.js');
     examples: ['!ban @Firulais', '!ban 556540723235651584', '!ban @Firulais Razon'],
     run: async (client , message, args) => {
 
-    let bUser = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
-    if(!bUser) return message.channel.send("Debes mencionar a un usuario o darme su id");
-    
+    let User = message.mentions.users.first() || client.users.cache.get(args[0]);
+    if(!User) return message.channel.send("Debes mencionar a un usuario o darme su id");
+    let bUser 
     let bReason = args.slice(1).join(" ");
     if(!bReason) bReason = "No se específico una razón"
+    if (message.guild.member(User)) {
     let role = bUser.roles.highest;
-    
+    bUser = message.guild.member(User)
     if(!message.member.hasPermission("BAN_MEMBERS" || "ADMINISTRATOR") || !message.guild.owner) return message.channel.send("No tienes permisos para usar este comando!");
     if(!message.guild.me.hasPermission(["BAN_MEMBERS" || "ADMINISTRATOR"])) return message.channel.send("No tengo permisos para Banear miembros");
     if(bUser.hasPermission("BAN_MEMBERS" || "ADMINISTRATOR") || !message.guild.owner) return message.channel.send("Esta persona no puede ser baneada!");
-
-    if (bUser.id === message.author.id) return message.channel.send("No te puedes banear a ti mismo")
+    if (User.id === message.author.id) return message.channel.send("No te puedes banear a ti mismo")
 
     if (message.guild.me.roles.highest.comparePositionTo(role) < 1) {
         return message.channel.send("Mi rol es muy bajo para banearlo!");
+    }
     }
 
     let banEmbed = new Discord.MessageEmbed()
     .setDescription("~Ban~")
     .setColor("#0088ff")
-    .addField("Usuario Baneado", `${bUser} Y su ID es ${bUser.id}`)
+    .addField("Usuario Baneado", `${User} Y su ID es ${User.id}`)
     .addField("Baneado Por", `<@!${message.author.id}> Y su ID es ${message.author.id}`)
     .addField("Razón de Baneo", bReason);
-    message.guild.member(bUser).ban(bReason);
+    message.guild.member(User).ban(bReason);
 
     message.channel.send( banEmbed )
     .catch(err => {
