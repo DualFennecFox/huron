9c7396bd9beebadcc88fb8be84b30c0c09f96d14
Firulais <gustavoperezsoto18@gmail.com>
2020-08-01 02:48:04 -0400

    Update warn.js

--- cmds/Moderacion/warn.js
+++ cmds/Moderacion/warn.js
@@ -78,7 +78,7 @@ const {search, updateGuild, createGuild } = require('./models/functions');
             db.save()
         }
         let number
-       if (doc.warnLevel) number = doc.warnLevel
+       if (doc) number = doc.warnLevel
        else number = 1
         warnLevel = parseInt(number)
         
