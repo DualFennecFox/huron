d3bdb8de3fbc049545bdbb8e871af7b85e234f6d
Firulais <gustavoperezsoto18@gmail.com>
2020-08-05 15:50:27 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -12,26 +12,10 @@ module.exports = async message => {
           if (!Channel) return
           if (!Channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return
 
-        let msg = ""
-        if (message.embeds) {
-            message.embeds.forEach((embeds) => {
-            if (embeds.description) msg = `**Descripción:** ${embeds.description}\n`
-            if (embeds.field) {
-               let field = embeds.fields.map(field => field.name).join("\n")
-                msg + ` **Campos:** ${field}`
-             }
-             if (embeds.footer) msg + ` **Pie:** ${embeds.footer}`
-             if (embeds.image) {
-                 msg + ` **Imagen:** [URL](${embeds.image.url})`
-             }
-            })
-        }
-        else msg = message.content
-
         const embed = new Discord.MessageEmbed()
         .setAuthor("Mensaje Eliminado", message.guild.iconURL())
         .setColor("#FF0000")
-        .setDescription(`**En:** <#${message.channel.id}>\n\n${msg}\n\n**Creado:**${checkDays(message.createdAt)}`)
+        .setDescription(`**En:** <#${message.channel.id}>\n\n${message.content}\n\n**Creado:**${checkDays(message.createdAt)}`)
         .setFooter(`De: ${message.author.tag} | ${message.author.id}`)
 
     Channel.send({ embed })
