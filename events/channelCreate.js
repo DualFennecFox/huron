6d98bb93fdc12e3d4c10ae386018f714056ef978
Firulais <gustavoperezsoto18@gmail.com>
2020-08-05 20:55:06 -0400

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -32,7 +32,7 @@ module.exports = async channel => {
       if (channel.permissionOverwrites) {
         perm = true
         for (const perm of channel.permissionOverwrites) {
-          console.log(perm[0].permissionOverwrites)
+          console.log(perm.type)
           if (perm[0].type === "member") {
             user = true
           overwritesAllowedUser.push(`<@!${perm[0].id}>: ${changeRole[perm[0].allow.toArray()]}`)
