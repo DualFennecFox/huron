918cdf8c9274a79b371494b84b8cd8117e2d8675
Firulais <gustavoperezsoto18@gmail.com>
2020-08-09 00:13:12 -0400

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -13,7 +13,7 @@ module.exports = async member => {
         if (!Channel) return
         if (!Channel.permissionsFor(member.guild.me).has("SEND_MESSAGES")) return
     
-        let msg = doc.LeaveMsg.replaceAll(/{user}/g, member).replace(/{server}/g, member.guild.name).replace(/{username}/g, member.user.tag).replace(/{members}/g, member.guild.memberCount).replace(/{owner}/g, member.guild.owner.user.tag)
+        let msg = doc.LeaveMsg.replace(/{user}/g, member).replace(/{server}/g, member.guild.name).replace(/{username}/g, member.user.tag).replace(/{members}/g, member.guild.memberCount).replace(/{owner}/g, member.guild.owner.user.tag)
     
         Channel.send(msg)
         }
