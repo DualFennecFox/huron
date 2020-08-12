00c06a18cba46b60231955a6baab114336f756c4
Firulais <gustavoperezsoto18@gmail.com>
2020-08-12 19:39:02 -0400

    Update roleUpdate.js

--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -30,18 +30,30 @@ module.exports = async (oldRole, newRole) => {
         }
         let newerperm = newRole.permissions.toArray()
         for (let v = 0; v < newRole.permissions.toArray().length; v++) {
-            if (!oldRole.permissions.has(newerperm[v], { checkAdmin: false })) {
+            if (!oldRole.permissions.has(newerperm[v])) {
                 newperm = true
-                let rol = changeRole[newerperm[v]]
-                getnewperm.push(rol)
+                if (!newRole.permissions.has('ADMINISTRATOR')) {
+                getnewperm.push("Administrador")
+                break;
+                }
+                else {
+                    let rol = changeRole[newerperm[v]]
+                    getnewperm.push(rol)
+                }
             }
           }
           let oldperm = oldRole.permissions.toArray()
         for (let v = 0; v < oldRole.permissions.toArray().length; v++) {
-            if (!newRole.permissions.has(oldperm[v], { checkAdmin: false })) {
+            if (!newRole.permissions.has(oldperm[v]) && oldperm[v] !== "ADMINISTRATOR") {
                 removeperms = true
-                let rol = changeRole[oldperm[v]]
-                getremoveperm.push(rol)
+                if (!oldRole.permissions.has("ADMINISTRATOR")) {
+                getremoveperm.push("Administrador")
+                break;
+                }
+                else {
+                    let rol = changeRole[oldperm[v]]
+                    getremoveperm.push(rol)
+                }
         }
         }
         if (oldRole.position != newRole.position) {
