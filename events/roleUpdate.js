66be89b9815d5f89dfcb42659f863980429d2a03
Firulais <gustavoperezsoto18@gmail.com>
2020-08-11 23:53:38 -0400

    Update roleUpdate.js

--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -28,6 +28,8 @@ module.exports = async (oldRole, newRole) => {
         if (oldRole.name != newRole.name) {
             name = true
         }
+        console.log(oldRole.permissions.toArray())
+        console.log(newRole.permissions.toArray())
         let newerperm = newRole.permissions.toArray()
         for (let v = 0; v < newRole.permissions.toArray().length; v++) {
             if (!oldRole.permissions.has(newperm[v])) {
