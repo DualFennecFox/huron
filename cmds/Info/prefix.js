29b6f14304950fd32237b44d6eb684afcb0ce8af
Firulais <gustavoperezsoto18@gmail.com>
2020-08-13 23:48:33 -0400

    prefix

--- cmds/Info/prefix.js
+++ cmds/Info/prefix.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "prefix",
-    category: "Moderacion",
-    description : "Con este comando puedes ver el prefix en el servidor",
+    category: "Info",
+    description : "Con este comando puedes ver el prefix en el servidor se cambia con !config prefix \"prefix\"",
     aliases: ['Prefix', 'PREFIX'],
     usage: '!prefix',
     examples: ['!prefix -', '!prefix --='],
