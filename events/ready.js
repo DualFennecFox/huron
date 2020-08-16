31a4d25189c333d6852893e05d5d28a4886f99fa
Firulais <gustavoperezsoto18@gmail.com>
2020-08-16 16:25:38 -0400

    http POST

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,13 @@
 const DBL = require("dblapi.js");
+const http = require('http');
+const querystring = require('querystring');
 
 module.exports = async client => {
     const dbl = new DBL(process.env.DBL, client)
     console.log(`Logged in as ${client.user.tag}!`);
     const scount = client.guilds.cache.size
-    dbl.postStats(client.guilds.cache.size)
+    dbl.postStats(scount)
+
     client.user.setPresence({
       status: "online",
       activity: {
@@ -13,4 +16,26 @@ module.exports = async client => {
           url: "https://www.twitch.tv/unfirulais"
       }
   }); 
+
+  var postData = querystring.stringify({
+    "guildCount": scount
+  })
+
+  var request = {
+    'host':`bots.ondiscord.xyz`,
+    'path': `/bot-api/bots/${client.user.id}/guilds`,
+    'method': "POST",
+    'auth': process.env.BOTS_ON_DISCORD,
+    headers: {
+      "Content_Type": "application/json"
+    }
+  }
+    let req = http.request(request, function (result) {
+      result.on('error', err => {
+        console.log("Hubo un error al subir los servidores a Bots on Discord: " + err)
+      })
+    })
+
+    req.write(postData);
+    req.end();
 }
