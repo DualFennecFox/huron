011ea162c7243d8a3a4efe397d938a7600dc1db9
Firulais <gustavoperezsoto18@gmail.com>
2020-08-22 23:42:43 -0400

    Update suggestion.js

--- cmds/util/suggestion.js
+++ cmds/util/suggestion.js
@@ -29,9 +29,10 @@ module.exports = {
         let suggestion = args.join(" ")
         
         const embed = new Discord.MessageEmbed()
-        .setAuthor(`Sugerencia #${level} Por ${message.author.tag}`, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+        .setAuthor(`Sugerencia #${level}`, message.author.displayAvatarURL({ format: "png", dynamic: true }))
         .setColor(color)
-        .addField("\u200b", suggestion)
+        .setDescription(suggestion)
+        .setFooter(`Por: ${message.author.tag} ID: ${message.author.id}`)
 
         channel.send({ embed })
 
