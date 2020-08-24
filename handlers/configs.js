31a54c9a88eaa257eb94fa1d4a3bd9f7b03d43ba
Firulais <gustavoperezsoto18@gmail.com>
2020-08-23 23:48:00 -0400

    Update configs.js

--- handlers/configs.js
+++ handlers/configs.js
@@ -1,10 +1,10 @@
 const { readdirSync } = require("fs");
 
 module.exports = (client) => {
-        const commands = readdirSync("../configs/").filter(file => file.endsWith(".js"));
+        const commands = readdirSync("./configs/").filter(file => file.endsWith(".js"));
     
         for (let file of commands) {
-            let pull = require(`../configs/${file}`);
+            let pull = require(`./configs/${file}`);
     
             if (pull.name) {
                 client.configs.set(pull.name, pull);
