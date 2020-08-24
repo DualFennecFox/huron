ec33e7aba9e4507cbd7913ecfe8bbecf27b62772
Firulais <gustavoperezsoto18@gmail.com>
2020-08-24 00:22:00 -0400

    Update config.js

--- cmds/Moderacion/config.js
+++ cmds/Moderacion/config.js
@@ -24,7 +24,7 @@ module.exports = {
                 .addField("MuteRole", `Para que funcione el mute se debe configurar un rol Muteado con este comando, se puede crear uno eligiendo un nombre y un color.\n**Uso:** ${prefix}config muterole <enable o disable> <Rol, ID o Nombre> [Color si se crea]\n`)
                 .addField("Suggestion", `Establece un canal de sugerencias, un color Hex para las sugerencias o ambos.\n**Uso:** ${prefix}config suggestion <enable o disable> <Canal o Color> <Canal o Color>`)
                 .addField("LogChannel", `Establece un canal para logear con su mención o ID\n**Uso:** ${prefix}config logchannel <enable o disable> <Canal>`)
-                .addField("Tags", "Los tags para los mensajes de bienvenida y despedida son:\n\n**{user}** : Menciona al usuario\n**{username}** : Muestra el nombre y el tag del usuario\n**{server}** : Muestra el nombre del servidor\n**{owner}** : Nombra al Owner del servidor con su tag\n**{members}** : Muestra el número de miembros desde que el usuario se unio o dejo el server.")
+                .addField("Tags", "Los tags para los mensajes de bienvenida y despedida son:\n\n**{user}** : Menciona al usuario\n**{username}** : Muestra el nombre y el tag del usuario\n**{server}** : Muestra el nombre del servidor\n**{owner}** : Nombra al Owner del servidor con su tag\n**{members}** : Muestra el número de miembros desde que el usuario se unio o dejo el server.\n")
                 .setFooter("<> es obligatorio, [] es opcional")
 
             return message.channel.send({ embed })
@@ -39,9 +39,10 @@ module.exports = {
             command = client.configs.get(cmd)
         }
 
+        if (!args[1] && cmd !== "reset") return message.channel.send("Ese no parece ser un bune uso del comando")
         if (args[1].toLowerCase() === "enable") method = args[1].toLowerCase()
         else if (args[1].toLowerCase() === "disable") method = args[1].toLowerCase()
-        else if (!method && cmd !== "prefix" || !method && cmd !== "reset") return message.channel.send("Ese no parece ser un buen uso del comando");
+        else if (cmd !== "prefix" || cmd !== "reset") return message.channel.send("Ese no parece ser un buen uso del comando");
 
         if (command) command.run(message, args, method)
         else return message.channel.send("Esa no es una configuración válida");
