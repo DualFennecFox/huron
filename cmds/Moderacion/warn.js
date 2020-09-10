3e9f7c1975e162dfb1615dbd3e0caa4586add2ff
Firulais <66933053+UnFirulais@users.noreply.github.com>
2020-09-09 21:49:22 -0400

    Cannot warn users with a highest role than you

--- cmds/Moderacion/warn.js
+++ cmds/Moderacion/warn.js
@@ -22,10 +22,13 @@ const {search, createGuild, getUser } = require('./models/functions');
 
     if (bUser.id === message.author.id) return message.channel.send("No te puedes advertir a ti mismo")
     if (bUser.id === client.user.id) return message.channel.send("No me puedo advertir a mi mismo")  
-    
+    if (message.guild.me.roles.highest.comparePositionTo(muterole) < 1) {
+    return message.channel.send("Tus roles no son lo suficientemente altos para advertir a este usuario");
+}
+
    let db = await Guild.findOne({ guildID: message.guild.id })
 
-   await Guild.findOne({ guildID: message.guild.id}, {warns: { $elemMatch: { warnUserID: bUser.id } } }).then((result) => {
+   Guild.findOne({ guildID: message.guild.id}, {warns: { $elemMatch: { warnUserID: bUser.id } } }).then((result) => {
 
     let warnLevel
     let doc = search(bUser.id, db.warns)
