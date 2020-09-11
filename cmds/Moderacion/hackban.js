68ba8f9918c0d3e2c7ce600574cf542d6fafbac4
Firulais <gustavoperezsoto18@gmail.com>
2020-09-11 16:47:08 -0400

    Update hackban.js

--- cmds/Moderacion/hackban.js
+++ cmds/Moderacion/hackban.js
@@ -28,7 +28,7 @@ module.exports = {
                 } catch (err) {
                     if (!user) return message.channel.send(`**${arg}** no es un usuario válido`)
             }
-            if (!user) return message.channel.send(`**${arg}** no es un usuario válido`)f
+            if (!user) return message.channel.send(`**${arg}** no es un usuario válido`)
     
             if (bans.find(u => u.user.id === user.id)) return message.channel.send(`<@${user.id}> Ya esta baneado`)
     
