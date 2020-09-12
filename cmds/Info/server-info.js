f15061cfd5d439818ccc682cd39b3b3f069383ed
Firulais <gustavoperezsoto18@gmail.com>
2020-09-12 14:46:20 -0400

    A

--- cmds/Info/server-info.js
+++ cmds/Info/server-info.js
@@ -70,7 +70,7 @@ module.exports = {
         .addField("Nivel de Verificación", verifLevels[message.guild.verificationLevel], true)
         .addField(channelName, `${channels} | ${channelOrder}`, true)
         .addField("Roles", message.guild.roles.cache.size, true)
-        .addField("Creado a las", `${message.guild.createdAt.toUTCString().substr(0, 16)} (${message.guild.createdAt.getDay()}/${message.guild.createdAt.getMonth()}/${message.guild.createdAt.getFullYear()})`, true)
+        .addField("Creado a las", `${message.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.guild.createdAt)})`, true)
         .setThumbnail(message.guild.iconURL())
     message.channel.send({embed})
     .catch(err => {
@@ -79,3 +79,4 @@ module.exports = {
 }
 }
 }
+zz
