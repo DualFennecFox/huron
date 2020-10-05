1a3010a8732269f5fc6d3c677a98f53daa955df2
Firulais <gustavoperezsoto18@gmail.com>
2020-10-04 20:57:19 -0400

    Update warn.js

--- cmds/Moderacion/warn.js
+++ cmds/Moderacion/warn.js
@@ -24,9 +24,6 @@ const {search, createGuild, getUser } = require('./models/functions');
     if (bUser.id === client.user.id) return message.channel.send("No me puedo advertir a mi mismo")  
 
     let muterole = bUser.roles.highest
-    if (message.member.roles.highest.comparePositionTo(muterole) < 1) {
-    return message.channel.send("Tus roles no son lo suficientemente altos para advertir a este usuario");
-}
 
    let db = await Guild.findOne({ guildID: message.guild.id })
 
