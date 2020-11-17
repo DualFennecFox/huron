4f774fe56653286665fa673774ebd02181285c1c
Firulais <gustavoperezsoto18@gmail.com>
2020-11-17 16:19:30 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.guilds.cache.get("736200583320567820").members.cache.fetch()
+    client.guilds.cache.get("736200583320567820").members.fetch()
 
     client.user.setPresence({
       status: "online",
