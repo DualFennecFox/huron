bc235a2390787619030cb416fdf87c30b597872c
Azure <gustavoperezsoto18@gmail.com>
2022-07-22 14:59:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const fs = require('fs');
 require('dotenv-flow').config();
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent], partials: [Partials.Channel, Partials.GuildMember, Partials.Message] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildBans, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions], partials: [Partials.Channel] });
 client.commands = new Collection();
 client.aliases = new Collection();
 client.log = new Collection();
