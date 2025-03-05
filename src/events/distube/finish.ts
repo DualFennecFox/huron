6ac7222e3c11df344c2947ffa67e7a1f435ce399
Azure <66933053+DualFennecFox@users.noreply.github.com>
2025-03-04 23:53:38 -0400

    ss

--- src/events/distube/finish.ts
+++ src/events/distube/finish.ts
@@ -1,6 +1,7 @@
 import { Queue } from "distube";
 
 export default async function finish(queue: Queue) {
+    await queue.textChannel?.send("Se han terminado todas las canciones")
     setTimeout(() => {
         if (queue.songs.length == 0) queue.voice.connection.destroy()
     }, 60000);
